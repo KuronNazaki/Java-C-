15334c4cb96e78aa80fefa26b2e4fe5bf9ca7b3f
KuronNazaki <akirahuynh2002@gmail.com>
2020-07-29 19:12:58 +0700

    First commit from HP

---
+++ Java/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Solution {
+    
+    static int HexaToDeci (final String hexaNumber) {
+        String alpha = "0123456789ABCDEF";
+        int length = hexaNumber.length();
+        int deciNumber = 0;
+
+        for (int i = 0; i < length; i++) {
+            deciNumber += alpha.indexOf(hexaNumber.charAt(i)) * Math.pow(16, length - i - 1);
+        }
+
+        return deciNumber;
+    }
+
+    static String DeciToHexa(final int deciNumber) {
+        String hexaNumber = "";
+        String alpha = "0123456789ABCDEF";
+
+
+        return hexaNumber;
+    }
+
+    public static void main(final String[] args) {
+
+        final String hexa = "BABE";
+        final int deci = HexaToDeci(hexa);
+        
+        System.out.println(deci);
+    }
+}
+
