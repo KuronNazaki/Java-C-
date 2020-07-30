0c3e29eb06194215dc7715e56067695e72cd3bf4
KuronNazaki <akirahuynh2002@gmail.com>
2020-07-30 11:51:37 +0700

    Add IO Data wit input file

--- Java/src/Solution.java
+++ Java/src/Solution.java
@@ -1,13 +1,21 @@
 package src;
-import data.*;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.*;
 
 public class Solution {
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException { 
+        Scanner scn = 
+            new Scanner(Paths.get("F:\\Huy\\Study\\Java\\Java & C#\\Java\\IO data\\input.txt"), 
+                            "UTF-8");
+        StringBuilder str;       
 
-        int deci = 79;
-        String hexa = Convert.DeciToHexa(deci);
-        
-        System.out.println(hexa);
+        while (scn.hasNext()) {
+            str = new StringBuilder(scn.nextLine());
+            System.out.println(str);
+        }
+        scn.close();  
     }
 }
 
